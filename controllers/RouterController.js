4a7f04304bae53081b9c615ded8ac5fab9a22f04
Luiz Fumes <luiz.fumes@dafiti.com.br>
2017-03-07 22:32:54 -0300

    Make a rebase after read it

--- controllers/RouterController.js
+++ controllers/RouterController.js
@@ -1,6 +1,16 @@
 'use strict';
 
 const Joi = require('joi');
+var cloudinary = require('cloudinary');
+
+cloudinary.config({ 
+  cloud_name: 'sample', 
+  api_key: '874837483274837', 
+  api_secret: 'a676b67565c6767a6767d6767f676fe1' 
+});
+
+//http://cloudinary.com/documentation/node_integration#getting_started_guide
+
 
 module.exports.getAllDocuments = (request, reply) => {
     reply({"ENV": process.env.NODE_ENV}).code(200);
